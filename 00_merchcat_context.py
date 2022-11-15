5f5efcfe4fc917c8b0cf65cada7010222ce82210
alexott <alexott>
2022-11-15 10:14:23 +0000

    Different fixes in markup & code

--- 00_merchcat_context.py
+++ 00_merchcat_context.py
@@ -20,7 +20,7 @@
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC &copy; 2021 Databricks, Inc. All rights reserved. The source in this notebook is provided subject to the Databricks License [https://databricks.com/db-license-source].  All included or referenced third party libraries are subject to the licenses set forth below.
+# MAGIC &copy; 2021 Databricks, Inc. All rights reserved. The source in this notebook is provided subject to the [Databricks License](https://databricks.com/db-license-source).  All included or referenced third party libraries are subject to the licenses set forth below.
 # MAGIC 
 # MAGIC | library                                               | description             | license    | source                                              |
 # MAGIC |-------------------------------------------------------|-------------------------|------------|-----------------------------------------------------|
--- config/configure_notebook.py
+++ config/configure_notebook.py
@@ -16,4 +16,4 @@ with open('config/application.yaml', 'r') as f:
 
 import mlflow
 username = dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get()
-mlflow.set_experiment('/Users/{}/{}'.format(username, config['model']['name']))
+mlflow.set_experiment(f"/Users/{username}/{config['model']['name']}"")
